f28203b13c4c32df00918ee4786958b5162d52cf
lucasbrito0611 <lucasbrito2006@hotmail.com>
2025-05-24 18:42:29 -0300

    Adicionado a home um link de tarefas (já tinha feito no commit anterior)

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
       <p className="text-lg sm:text-xl text-gray-600 dark:text-gray-300 mb-8 max-w-xl">
         Este é um projeto inicial usando a estrutura App Router do Next.js com suporte a Tailwind CSS, com direito a página de TAREFAS.
       </p>
+      {/* LINK */}
       <Link
         href="/tarefas"
         className="px-6 py-3 bg-black text-white rounded-full text-sm sm:text-base font-medium hover:bg-gray-800 transition dark:bg-white dark:text-black dark:hover:bg-gray-300"
